d5f0941631a145caa38839b96e4ed93e57e3490f
Robert-App-Dev <robert01.appdev@gmail.com>
2022-03-25 17:45:47 +0000

    Singleton

---
+++ Singleton/Singleton/MySingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class MySingleton
+    {
+        private MySingleton() { } // Prevents instantiation
+
+        private static MySingleton instance = new MySingleton();
+
+        public static MySingleton getInstance()
+        {
+            return instance;
+        }
+
+
+        private int counter = 0;
+
+        public void count()
+        {
+            Console.WriteLine(counter);
+            counter++;
+        }
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// Singletons could be very useful in Unity where some features such as a server connection manager will only exist once
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MySingleton this_should_fail = new MySingleton(); - Inaccessible due to protection level
+
+            MySingleton single = MySingleton.getInstance();
+
+            single.count();
+            single.count();
+            single.count();
+        }
+    }
+}
