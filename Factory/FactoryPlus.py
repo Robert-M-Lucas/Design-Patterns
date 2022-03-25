cf9f2417e378b49600986e2c1b9c510c75c3db1c
Robert Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2022-03-25 10:47:45 +0000

    Add files via upload

---
+++ Factory/FactoryPlus.py
@@ -0,0 +1,26 @@
+# Could be used in a game where living and undead versions of characters are very distinct
+
+class AbstractCharFactoryFactory:
+    pass
+
+class CharFactory:
+    pass
+
+class UndeadCharFactory(CharFactory):
+    pass
+
+class LivingCharFactory(CharFactory):
+    pass
+
+class Character:
+    pass
+
+class LivingHuman(Character):
+    pass
+
+class UndeadHuman(Character):
+    pass
+
+class LivingDog(Character):
+    pass
+
