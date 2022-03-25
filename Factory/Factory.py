f0e37fc94daa24f51a22917cb4f58cf3903fa8b9
Robert Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2022-03-25 10:15:02 +0000

    Factory

---
+++ Factory/Factory.py
@@ -0,0 +1,27 @@
+
+
+class Car:
+    def get_wheels(self) -> int:
+        return -1
+    
+class MiniCar(Car):
+    def get_wheels(self) -> int:
+        return 4
+    
+class LorryCar(Car):
+    def get_wheels(self) -> int:
+        return 12
+    
+class CarFactory:
+    def get_car(self, brand) -> Car:
+        if brand == "Mini":
+            return MiniCar()
+        elif brand == "Lorry":
+            return LorryCar()
+        return None
+    
+car1 = CarFactory().get_car("Mini")
+print(car1.get_wheels())
+
+car2 = CarFactory().get_car("Lorry")
+print(car2.get_wheels())
